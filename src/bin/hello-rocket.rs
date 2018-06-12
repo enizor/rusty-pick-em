90c8dfd663125c4fc8cd03f78126615b88830e09
Rémi Garde <remi.garde@free.fr>
2018-06-12 22:34:32 +0200

    Feeat: Login GET/POST tests

--- src/bin/hello-rocket.rs
+++ src/bin/hello-rocket.rs
@@ -1,7 +1,16 @@
 #![feature(plugin)]
+#![feature(custom_derive)]
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate rocket_contrib;
+
+use rocket::Request;
+use rocket::response::Redirect;
+use rocket::request::Form;
+use rocket_contrib::Template;
+
+#[macro_use] extern crate serde_derive;
 
 use std::io::Result;
 use std::fs::File;
@@ -18,6 +27,26 @@ fn files(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("vue/dist/").join(file)).ok()
 }
 
+#[get("/login")]
+fn login() -> Result<NamedFile> {
+    NamedFile::open("front/login.html")
+}
+
+#[derive(FromForm, Serialize)]
+struct AuthUser {
+    name: String,
+    password: String,
+}
+
+#[post("/login", data = "<auth_user>")]
+fn authUser(auth_user: Form<AuthUser>) -> Template {
+    Template::render("test", &auth_user.get())
+}
+
+
+
 fn main() {
-    rocket::ignite().mount("/", routes![index, files]).launch();
+    rocket::ignite().mount("/", routes![index, files, login, authUser])
+    .attach(Template::fairing())
+    .launch();
 }
